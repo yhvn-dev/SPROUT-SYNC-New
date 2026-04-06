3291d1fa5b06dcfff345be684b9c5af9ddad5c04
yhun <jhunguidef@gmail.com>
2026-04-07 02:54:27 +0800

    try fix

--- server/src/routes/ProtectedRoutes/deviceToken.Routes.js
+++ server/src/routes/ProtectedRoutes/deviceToken.Routes.js
@@ -4,9 +4,9 @@ import { verifyAccessToken } from "../../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-    router.get("/get/deviceToken/all",verifyAccessToken,deviceTokenController.fetchAllUserDevices);
-    router.get("/get/deviceToken/:user_id",verifyAccessToken,deviceTokenController.fetchUserDevices);
-    router.post("/post/deviceToken/register",verifyAccessToken,deviceTokenController.registerDevice);
+    router.get("/get/deviceToken/all",deviceTokenController.fetchAllUserDevices);
+    router.get("/get/deviceToken/:user_id",deviceTokenController.fetchUserDevices);
+    router.post("/post/deviceToken/register",deviceTokenController.registerDevice);
     router.delete("/delete/all/deviceToken",verifyAccessToken,deviceTokenController.removeAllDevice);
     router.delete("/delete/deviceToken",verifyAccessToken,deviceTokenController.removeDevice);
  
